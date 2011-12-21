9b44f7cf93862aaf55ee12f03f161b559eef6cfe
mirar.cmd@gmail.com <mirar.cmd@gmail.com@7558c97a-468f-df93-ee3b-8be943886b77>
2011-12-21 22:28:27 +0000

    Diverse Fehler in der Rechten Seite (RHS.m) wurden behoben

--- branches/MATLAB/kreisel.m
+++ branches/MATLAB/kreisel.m
@@ -2,5 +2,7 @@ close all;
 clear all;
 clc;
 % x = [my; lambda; ny; xi; omikron; theta; phi; psi; xc; yc]
-x0 = [0 0 250 0 0 0.1 0 0 0 0];  
-[wi ti] = rkf45(@RHS,0,x0',3,[1E-22 1E-2 1E-2]);
+x0 = [0 0 250 0 0 0.1 0 0 0 0];
+tic;
+[wi ti] = rkf45(@RHS,0,x0',0.5,[1E-22 1E-6 1E-8]);
+runtime = toc;
