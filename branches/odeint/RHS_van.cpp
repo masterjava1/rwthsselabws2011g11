1fc061247c8776ed5ac03c14f9c15a9452f9b438
mirar.cmd@googlemail.com <mirar.cmd@googlemail.com@7558c97a-468f-df93-ee3b-8be943886b77>
2012-03-31 13:46:12 +0000

    cleaning up

--- branches/odeint/RHS_van.cpp
+++
@@ -1,3 +0,0 @@
-#include "RHS_van.h"
-
-RHS_van::RHS_van(double epss) : eps(epss) {}
--- branches/odeint/StepperBase.cpp
+++
@@ -1,6 +0,0 @@
-#include "StepperBase.h"
-
-StepperBase::StepperBase(Array<double,1> &yy, Array<double,1> &dydxx, double &xx,
-const double atoll, const double rtoll, bool dens)
-: x(xx), y(yy), dydx(dydxx), atol(atoll), rtol(rtoll), dense(dens),
-  n(y.size()), neqn(n), yout(n), yerr(n){}
